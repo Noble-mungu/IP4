7f6a9feefe76c3da40180d4d71fcbec0fd2080e6
Noble <mungunoble@gmail.com>
2020-11-03 10:08:21 +0300

    configure db

--- hoodwatch/settings.py
+++ hoodwatch/settings.py
@@ -46,8 +46,8 @@ INSTALLED_APPS = [
     'pyuploadcare.dj',
 ]
 UPLOADCARE = {
-    'pub_key': os.environ.get('PUB_KEY'),
-    'secret': os.environ.get('SECRET'),
+    PUB_KEY='0fe8c59ceb318d246c4d'
+    SECRET='2b04398f86bf81d225bc'
 }
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
@@ -85,14 +85,24 @@ WSGI_APPLICATION = 'hoodwatch.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': os.environ.get('DB_PORT'),
+#     },
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME':'hoodwatch' ,
+        'USER': 'postgres',
+        'PASSWORD': 'azbycx567',
+        'HOST': localhost,
+        'PORT': '5432',
     },
 }
 
